d6f01ea6a4aee2b6dd3c24c038ccb3c9cfe30a62
KOMPROMISSS <119791141+KOMPROMISSS@users.noreply.github.com>
2023-05-29 01:10:08 +0300

    н

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
-const elementsLike = document.querySelectorAll(".elements__like-button"),
+const 
+    // elementsLike = document.querySelectorAll(".elements__like-button"),
     nameInput = document.querySelector("#name-input"),
     descriptionInput = document.querySelector("#description-input"),
     profileTitle = document.querySelector(".profile__title"),
     profileSubtitle = document.querySelector(".profile__subtitle"),
     profileEditButton = document.querySelector(".profile__edit-button"),
     popupCloseButton = document.querySelector(".popup__close-button"),
-    popupSaveButton = document.querySelector(".popup__save-button"),
+    // popupSaveButton = document.querySelector(".popup__save-button"),
     popup = document.querySelector(".popup");
 
 profileEditButton.addEventListener("click", function () {
@@ -25,14 +26,14 @@ popupSaveButton.addEventListener("click", () => {
     profileSubtitle.textContent = newProfileSubtitle;
 })
 
-elementsLike.forEach(element => {
-    element.addEventListener("click", () => {
-        if (element.classList.value == "elements__like-button") {
-            element.classList.add("elements__like-button_active")
-            element.classList.remove("elements__like-button")
-        } else {
-            element.classList.add("elements__like-button")
-            element.classList.remove("elements__like-button_active")
-        }
-    })
-});
+// elementsLike.forEach(element => {
+//     element.addEventListener("click", () => {
+//         if (element.classList.value == "elements__like-button") {
+//             element.classList.add("elements__like-button_active")
+//             element.classList.remove("elements__like-button")
+//         } else {
+//             element.classList.add("elements__like-button")
+//             element.classList.remove("elements__like-button_active")
+//         }
+//     })
+// });
